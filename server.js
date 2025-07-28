a9fedd16b7b03e42c9c76eeb3baf3615273fd1b6
jiy0712 <s2325@e-mirim.hs.kr>
2025-07-28 21:25:21 +0900

    chore : 서버 / 라우터 연결

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
+//라우터
 //추후 '././' 경로 넣기
 const storageRouter = require();
 const imgRouter = require();
 const qrRouter = require();
 
+app.use(cors());
+app.use(express.json());
+
+app.use('/storage', express.static(path.join(__dirname, 'storage')));
+
+app.use('/storage', storageRouter);
+app.use('/img', imgRouter);
+app.use('/qr', qrRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`서버 : ${PORT}`);
+})

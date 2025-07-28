c6a642e432568f5c12d98078c5b20644397662b1
jiy0712 <s2325@e-mirim.hs.kr>
2025-07-28 21:06:28 +0900

    첫 커밋

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+
+//추후 '././' 경로 넣기
+const storageRouter = require();
+const imgRouter = require();
+const qrRouter = require();
+

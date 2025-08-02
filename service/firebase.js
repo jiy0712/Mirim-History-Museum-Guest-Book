952e3380901ad5c9bdade4d41f7943e45bc45862
jiy0712 <s2325@e-mirim.hs.kr>
2025-08-02 19:35:43 +0900

    feat : firebase 관련 업로드 기능 완료

--- service/firebase.js
+++ service/firebase.js
@@ -4,20 +4,14 @@ const admin = require('firebase-admin');
 const path = require('path');
 const fs = require('fs');
 
-console.log('firebase.js GOOGLE_APPLICATION_CREDENTIALS:', process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
 //firebase admin sdk 초기화
-// const adminsdk = require('../mirim-history-museum-e3f30-firebase-adminsdk-fbsvc-2785f78949.json');
-
 admin.initializeApp({
-    // credential: admin.credential.cert(adminsdk),
     credential: admin.credential.applicationDefault(),
     storageBucket: 'mirim-history-museum-e3f30.firebasestorage.app'
 });
 
 const bucket = admin.storage().bucket();
-console.log('버킷이름테스트 : ', bucket.name);
-console.log("GOOGLE_APPLICATION_CREDENTIALS:", process.env.GOOGLE_APPLICATION_CREDENTIALS);
 //firebase에 이미지 저장
 async function uploadImg(fileBuffer, destination) {
     try{
@@ -25,12 +19,11 @@ async function uploadImg(fileBuffer, destination) {
 
         await file.save(fileBuffer, {
             metadata: {
-                contentType: 'image/png', //설정 바꿀 수 있음
+                contentType: 'image/png', //사진 종류에 따라 설정 변경
             },
             public: true,
         });
 
-        //수정 확인 필요
         const publicUrl = `https://storage.googleapis.com/${bucket.name}/${destination}`;
         return publicUrl;
     } catch(err){
